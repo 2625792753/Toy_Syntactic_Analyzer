a11e981859c3eb4bd59c0e350f5fae67dcfd40df
ZOE <43523594+2625792753@users.noreply.github.com>
2023-06-15 19:54:22 +0800

    ok

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char const *argv[])
 {
-    if (generate_rulelist("../rules.txt"))
+    if (generate_rulelist("../../rules.txt"))
     {
 #ifdef _DEBUG_
         cout << "DEBUG_INFO:: " << endl;
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[])
         generate_all_vessels();
     }
 
-    if (load_input("../input.txt"))
+    if (load_input("../../input.txt"))
     {
         if (syntactic_analyze(input))
             cout << "yes";
